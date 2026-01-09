f44b6ba9042111fd6a2349f764c709c024062d04
Tên của bạn <email@example.com>
2026-01-09 11:33:00 +0700

    update

--- Slot 2/khaibaobien.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	int so;
-	so = 100;
-	printf("%d\n", so);
-	return 0;
-}
--- Slot 2/sothuc.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	float sothuc1 = 1.2357;
-	double sothuc2 = 1.23;
-	printf("So thuc1: %.2f\n", sothuc1);
-	printf("So thuc2: %lf", sothuc2);
-	return 0;
-}
