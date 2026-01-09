fd78577b1b8f25c56ac9f3a9b0e3e6fa5385a7bd
Tên của bạn <email@example.com>
2026-01-09 11:26:32 +0700

    update

--- Slot 2/bai2.c
+++ Slot 2/bai2.c
@@ -4,6 +4,7 @@ int main() {
 	double a = 5;
 	double b = 10.216;
 	double sum = a + b;
-	printf("a = %.2lf\nb = %.2lf\nTong = %.2lf", a, b, sum);
+	printf("a = %.2lf\nb = %.2lf\n", a, b);
+	printf("%.2lf + %.2lf = %.2lf", a, b, sum);
 	return 0;
 }
