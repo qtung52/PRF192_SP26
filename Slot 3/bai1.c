cdc32a39a83f857268dce84d5fe35789f2f11482
qtung52 <quangtung050206@gmail.com>
2026-01-14 08:40:15 +0700

    update

--- Slot 3/bai1.c
+++ Slot 3/bai1.c
@@ -4,11 +4,12 @@ int main() {
 	int a,b;
 	a = 7;
 	b = 2;
+	float kq;
 	printf("%-15s %-15s %-15s %-15s %-15s\n", "A", "Phep tinh", "B", "Bang", "Ket qua");
 	printf("-----------------------------------------------------------------------\n");
-	printf("%-15d %-15c %-15d %-15c %-15d\n", a,'+', b,'=', a + b);
-	printf("%-15d %-15c %-15d %-15c %-15d\n", a,'-', b,'=', a - b);
-	printf("%-15d %-15c %-15d %-15c %-15d\n", a,'*', b,'=', a * b);
-	printf("%-15d %-15c %-15d %-15c %-15.1f\n", a,'/', b,'=', (float)a / b);
+	printf("%-15d %-15c %-15d %-15c %-15.0f\n", a,'+', b,'=', kq = a + b);
+	printf("%-15d %-15c %-15d %-15c %-15.0f\n", a,'-', b,'=', kq = a - b);
+	printf("%-15d %-15c %-15d %-15c %-15.0f\n", a,'*', b,'=', kq = a * b);
+	printf("%-15d %-15c %-15d %-15c %-15.1f\n", a,'/', b,'=', kq = (float)a / b);
 	return 0;
 }
--- Slot 3/bai2.c
+++ Slot 3/bai2.c
@@ -2,8 +2,8 @@
 
 int main() {
 	char kytu = 't';
-	printf("%-15s %-15s %-15s %-15s %-15s\n", "Gtri", "Gtri thap phan", "Gtri hex", "Gtri octal", "Kich thuoc");
-	printf("--------------------------------------------------------------------------\n");
-	printf("%-15c %-15d %-15x %-15o %-15d", kytu, kytu, kytu, kytu, sizeof(kytu));
+	printf("%-15s %-15s %-15s %-15s %-15s %-15s\n","Ten bien", "Gtri", "Gtri thap phan", "Gtri hex", "Gtri octal", "Kich thuoc");
+	printf("------------------------------------------------------------------------------------------\n");
+	printf("%-15s %-15c %-15d %-15x %-15o %-15d","kytu", kytu, kytu, kytu, kytu, sizeof(kytu));
 	return 0;
 }
