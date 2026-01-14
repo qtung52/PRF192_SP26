3caa211d8fcb1cb3bc5f156d73338d8ed9ef033f
qtung52 <quangtung050206@gmail.com>
2026-01-14 08:28:55 +0700

    update

--- Slot 3/bai2.c
+++ Slot 3/bai2.c
@@ -3,6 +3,7 @@
 int main() {
 	char kytu = 't';
 	printf("%-15s %-15s %-15s %-15s %-15s\n", "Gtri", "Gtri thap phan", "Gtri hex", "Gtri octal", "Kich thuoc");
+	printf("--------------------------------------------------------------------------\n");
 	printf("%-15c %-15d %-15x %-15o %-15d", kytu, kytu, kytu, kytu, sizeof(kytu));
 	return 0;
 }
