e22faeeac55fe4598bc0ec35db40501d22dc8b75
qtung52 <quangtung050206@gmail.com>
2026-01-14 08:26:10 +0700

    update slot 3

---
+++ Slot 3/bai1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int a,b;
+	a = 7;
+	b = 2;
+	printf("%-15s %-15s %-15s %-15s %-15s\n", "A", "Phep tinh", "B", "Bang", "Ket qua");
+	printf("%-15d %-15c %-15d %-15c %-15d\n", a,'+', b,'=', a + b);
+	printf("%-15d %-15c %-15d %-15c %-15d\n", a,'-', b,'=', a - b);
+	printf("%-15d %-15c %-15d %-15c %-15d\n", a,'*', b,'=', a * b);
+	printf("%-15d %-15c %-15d %-15c %-15.1f\n", a,'/', b,'=', (float)a / b);
+	return 0;
+}
---
+++ Slot 3/bai2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+	char kytu = 't';
+	printf("%-15s %-15s %-15s %-15s %-15s\n", "Gtri", "Gtri thap phan", "Gtri hex", "Gtri octal", "Kich thuoc");
+	printf("%-15c %-15d %-15x %-15o %-15d", kytu, kytu, kytu, kytu, sizeof(kytu));
+	return 0;
+}
