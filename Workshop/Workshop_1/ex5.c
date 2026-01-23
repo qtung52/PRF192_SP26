dfb18c0e0b84ae397e839c2094ebd9b2d45d584a
qtung52 <quangtung050206@gmail.com>
2026-01-23 14:05:34 +0700

    update

--- Workshop/Workshop_1/ex5.c
+++ Workshop/Workshop_1/ex5.c
@@ -3,30 +3,34 @@
 int main() {
 	float soKm;
 	int soPhutCho;
-	float money;
-	int tongTien;
+	float tongTien;
+	
 	printf("Nhap so Km: ");
 	if(scanf("%f", &soKm) != 1 || soKm <= 0) {
 		printf("Nhap sai!");
 		return 0;
 	}
+	
 	printf("Nhap thoi gian cho: ");
 	if(scanf("%d", &soPhutCho) != 1 || soPhutCho < 0) {
 		printf("Nhap sai!");
 		return 0;
 	}
+	
 	if(soKm <= 0.5) {
-		money = 12000;
+		tongTien = 12000;
 	} else if(soKm <= 30) {
-		money = 12000 + (soKm - 0.5) * 15000;
+		tongTien = 12000 + (soKm - 0.5) * 15000;
 	} else {
-		money = 12000 + 29.5 * 15000 + (soKm - 30) * 12000;
+		tongTien = 12000 + 29.5 * 15000 + (soKm - 30) * 12000;
 	}
-	tongTien = money;
+	
 	if (soPhutCho > 5) {
-		tongTien =  ((soPhutCho - 5) * 1000) + money;
+		tongTien += ((soPhutCho - 5) * 1000);
 	}
+	
 	printf("%-5s | %-15s | %-10s\n", "So Km", "So phut cho", "Tong tien");
-	printf("%-5.2f | %-15d | %-10d", soKm, soPhutCho, tongTien);
+	printf("%-5.2f | %-15d | %-10.0f", soKm, soPhutCho, tongTien);
+	
 	return 0;
 }
