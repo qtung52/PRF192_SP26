a64820d1b1cda19f127b617e16585f74ffa661a6
qtung52 <quangtung050206@gmail.com>
2026-01-28 07:59:55 +0700

    update

--- Workshop/Workshop_1/ex6.c
+++ Workshop/Workshop_1/ex6.c
@@ -5,33 +5,21 @@ int main () {
 	float tong;
 	char diemChu;
 	
-	printf("Nhap diem chuyen can: ");
-	if(scanf("%f", &chuyenCan) != 1 || chuyenCan < 0) {
+	printf("Nhap diem: ");
+	if(scanf("%f %f %f", &chuyenCan, &giuaKy, &cuoiKy) != 3 || chuyenCan < 0 || giuaKy < 0 || cuoiKy < 0) {
 		printf("Nhap sai diem!");
 		return 0;
 	}
 	
-	printf("Nhap diem giua ky: ");
-	if(scanf(" %f", &giuaKy) != 1 || giuaKy < 0) {
-		printf("Nhap sai diem!");
-		return 0;
-	}
-	
-	printf("Nhap diem cuoi ky: ");
-	if(scanf(" %f", &cuoiKy) != 1 || cuoiKy < 0) {
-		printf("Nhap sai diem!");
-		return 0;
-	}
-	
-	tong = (chuyenCan * 0.1) + (giuaKy * 0.3) + (giuaKy * 0.6);
+	tong = (chuyenCan * 0.1) + (giuaKy * 0.3) + (cuoiKy * 0.6);
 	
 	if(tong >= 8.5 && tong <=10) {
 		diemChu = 'A';
-	} else if(tong >= 7 && tong < 8.5) {
+	} else if(tong >= 7) {
 		diemChu = 'B';
-	} else if(tong >= 5.5 && tong < 7) {
+	} else if(tong >= 5.5) {
 		diemChu = 'C';
-	} else if(tong >= 4 && tong < 5.5) {
+	} else if(tong >= 4) {
 		diemChu = 'D';
 	} else {
 		diemChu = 'F';
