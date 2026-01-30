dd8f735dfc41dc0394d80b8770a997e4ed78061b
qtung52 <quangtung050206@gmail.com>
2026-01-30 11:43:58 +0700

    update

---
+++ Slot_7/bai1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+	int thang;
+	int quy;
+	printf("Nhap thang: ");
+	int kqNhap = scanf("%d", &thang);
+	if(kqNhap != 1 || thang < 1 || thang > 12) {
+		printf("Nhap sai thang");
+		return 0;
+	}
+	
+	switch(thang) {
+		case 1:
+		case 2:
+		case 3:
+			quy = 1;
+			break;
+		case 4:
+		case 5:
+		case 6:
+			quy = 2;
+			break;
+		case 7:
+		case 8:
+		case 9:
+			quy = 3;
+			break;
+		default:
+			quy = 4;
+			break;
+	}
+	
+	printf("Thang: %d \nQuy: %d", thang, quy);
+	return 0;
+}
---
+++ Slot_7/bai2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+	int so1, so2;
+	float kq;
+	char phepTinh;
+	
+	scanf("%d, %d", &so1, &so2);
+	fflush(stdin);
+	scanf("%c", &phepTinh);
+	
+	switch(phepTinh) {
+		case '+':
+			kq = so1 + so2;
+			break;
+		case '-':
+			kq = so1 - so2;
+			break;
+		case '*':
+			kq = so1 * so2;
+			break;
+		case '/':
+			if(so2 == 0) {
+				printf("Khong the chia cho 0");
+				return 0;
+			} else
+				kq = (float)so1 / so2;
+			break;
+		case '%':
+			if(so2 == 0) {
+				printf("Khong the chia cho 0");
+				return 0;
+			}
+			kq = so1 % so2;
+			break;
+		default:
+			printf("Nhap sai phep tinh");
+			return 0;
+	}
+	
+	printf("Phep tinh: %d %c %d = %.2f", so1, phepTinh, so2, kq);
+	return 0;
+}
